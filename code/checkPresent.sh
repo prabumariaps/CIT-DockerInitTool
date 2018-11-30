796c861cc6edb17d855972570c3be76d23d37deb
Hieu Le <lphieu@tma.com.vn>
2018-11-30 13:53:23 +0700

    update checkcode fix bug

--- code/checkPresent.sh
+++ code/checkPresent.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 cd ./web || exit
-if ! ls *.php -l | wc -l; then
+if ! ls *.php -l | wc -l || false; then
     echo "PHP Work!"
 fi
 
 cd ./python || exit
-if ! ls *.python -l | wc -l; then
+if ! ls *.py -l | wc -l || false; then
     echo "Python Work!"
 fi
 
 cd ./java || exit
-if ! ls *.java -l | wc -l; then
+if ! ls *.java -l | wc -l || false; then
     echo "Java Work!"
 fi
